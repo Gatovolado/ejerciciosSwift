39096bcc4e3bd4669dd08beea8648b54206f1cf6
GatoManzana <carlos_sanchez_m-tsapp1ma2324@gmail.com>
2024-10-09 09:18:18 +0100

    ejercicio 5 realizado

--- ejercicios/ejercicio5.swift
+++ ejercicios/ejercicio5.swift
@@ -1,8 +1,13 @@
-//
-//  ejercicio5.swift
-//  ejercicios
-//
-//  Created by user234561 on 9/10/24.
-//
+func ej5(){
+    let numRom = ["I":1, "V":5, "X": 10, "L":50, "C":100, "D":500, "M":1000]
+    print ("Introduce un número romano para traducir a decimal:\n")
+    let num = readLine()!
+    var total = 0
+    for i in num{
+        let letra = String(i)
+        let numUsuario = numRom[letra] ?? 0
+        total = total + numUsuario
+    }
+    print (total)
+}
 
-import Foundation
