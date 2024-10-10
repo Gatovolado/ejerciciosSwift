26e0b61f2c2283015e3b68a1dc97550814d11724
GatoManzana <carlos_sanchez_m-tsapp1ma2324@gmail.com>
2024-10-10 13:21:38 +0100

    ejercicio 6 a la mitad

--- ejercicios/ejercicio6.swift
+++ ejercicios/ejercicio6.swift
@@ -2,8 +2,22 @@ func ej6() {
     let numRom = ["I":1, "V":5, "X": 10, "L":50, "C":100, "D":500, "M":1000]
     print ("Introduce un número romano para traducir a decimal:\n")
     let num = readLine()!
+    var numAnterior = 0
     var total = 0
     for i in num.reversed(){
+        let letra = String(i)
+        var numUsuario = numRom[letra] ?? 0
+        print("num siguiente:",numUsuario)
+        if numAnterior < numUsuario{
+            numAnterior = numUsuario
+            
+            print("numero anterior:",numAnterior)
+        }else if numUsuario < numAnterior{
+            numUsuario = numAnterior - numUsuario
+            total = total + numUsuario
+            print(numUsuario)
+        }
         
     }
+    print("el numero en decimal es:",total)
 }
